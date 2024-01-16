d2733a86febcef630d9c02d27e79550a76e0d317
zzhangblog <zhzhangwork@qq.com>
2024-01-16 23:54:31 +0800

    Site updated: 2024-01-16 23:54:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/15/hello-world/","2023/12/06/data/","2023/12/15/software-win-tool/"];function toRandomPost(){
+var posts=["2023/12/06/data/","2023/12/15/hello-world/","2023/12/15/software-win-tool/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
