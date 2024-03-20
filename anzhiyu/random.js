3861f76e0a333fcff4363091ed3e49f8599e916a
zzhangblog <zhzhangwork@qq.com>
2024-03-20 12:10:32 +0800

    Site updated: 2024-03-20 12:10:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/15/hello-world/","2023/12/15/software-win-tool/","2024/01/23/vedio recoding/"];function toRandomPost(){
+var posts=["2024/03/20/game-ori/","2024/03/20/hello-world/","2024/03/20/software-win-tool/","2024/01/23/vedio recoding/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
