d483f289d4519bb8d06d3ad130ca2fdba9c1b780
zzhangblog <zhzhangwork@qq.com>
2024-03-21 12:12:26 +0800

    Site updated: 2024-03-21 12:12:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/20/software-win-tool/","2024/03/21/game-summary/","2024/01/23/vedio recoding/","2024/03/20/game-ori/","2024/03/20/hello-world/"];function toRandomPost(){
+var posts=["2024/03/20/game-ori/","2024/03/20/hello-world/","2024/01/23/vedio recoding/","2024/03/21/game-summary/","2024/03/20/software-win-tool/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
